2bc2f12be961c74081ac0d58bb9e4c20e32256c7
mcarmenrs <mcarmen.rs79@gmail.com>
2023-04-05 17:00:55 +0000

    start button

--- main.js
+++ main.js
@@ -13,10 +13,35 @@ form.addEventListener('submit', e => {
     if(itTask.value != ''){
         createTask(itTask.value);
         itTask.value = '';
+        renderTasks();
     }
 });
 
 function createTask(value){
+    const newTask = {
+        id: (Math.random() * 100).toString(36).slice(3),
+        title: value,
+        completed: false,
+    };
 
-    
+    tasks.unshift(newTask);
+}
+
+function renderTasks() {
+    const html = tasks.map(tasks => {
+        return `
+            <div class="task">
+                <div class="completed">${tasks.completed ? `<span class="done">Done</span>` :`<button class="start-button" data-id="${tasks.id}">Start</button>`}</div>
+                <div class="title">${tasks.title}</div>
+            </div>
+
+        `;
+    });
+
+    const tasksContainer = document.querySelector('#tasks');
+    tasksContainer.innerHTML = html.join("");
+
+    const startButtons = document.querySelectorAll('.task .start-button');
+
+    startButtons.forEach
 }
